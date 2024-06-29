07c4da736a1e913e42f03e1f06f97f31b4da5784
Anna <zhukova@tutu.tech>
2024-06-29 16:51:21 +0300

    Initial Commit

---
+++ TarantinoFilms/TarantinoFilmsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TarantinoFilmsApp.swift
+//  TarantinoFilms
+//
+//  Created by Zhukova on 29.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct TarantinoFilmsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
